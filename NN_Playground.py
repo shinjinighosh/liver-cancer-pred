c485adc03b04b6a92368ef6eb46dae4dc85847d1
Shinjini Ghosh <shinghos@mit.edu>
2020-06-07 20:26:52 -0400

    need to preprocess data before fitting model

--- NN_Playground.py
+++ NN_Playground.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import warnings
 import pandas as pd
 import numpy as np
@@ -18,4 +20,18 @@ X = data[['Age', 'Gender', 'Total_Bilirubin', 'Direct_Bilirubin', 'Alkaline_Phos
           'Alamine_Aminotransferase', 'Aspartate_Aminotransferase', 'Total_Protiens', 'Albumin', 'Albumin_and_Globulin_Ratio']]
 Y = data[['Dataset']]  # labels
 
+# preprocessing
+
+
 # building model
+model = Sequential()
+input_dims = X.shape  # should be (583, 10)
+model.add(Dense(10, activation='relu', input_dim=input_dims[1]))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# compiling model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# fitting model on data
+model.fit(X, Y, epochs=10, batch_size=20)
